0279d686a1781bfb60cbf15785976d1cfd664a17
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-24 15:20:28 +0300

    commit tree 51c019c7cc318e3402f85f9277a9b1dbe1423214

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+end
+return 'HAPPY NEW YEAR!'
+end
+#countdown(5)
+def countdown_with_sleep(number)
+while number > 0
+    puts "#{number} SECOND(S)!\n"
+    sleep 1
+    number -= 1
+end
+puts "HAPPY NEW YEAR!\n"
+
 end
